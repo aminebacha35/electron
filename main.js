7974ae1279d9368f73d4f2cf2d695839171ab288
amine <amine.bacha50@gmail.com>
2024-12-09 16:10:01 +0100

    update electron

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 const{updateElectronApp} = require('update-electron-app')
-updateElectronApp
+updateElectronApp();
 
 const createWindow = () => {
   // Create the browser window.
